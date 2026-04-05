3d92000f64dc6d049f260b23f30aa5f27a8eb6ca
znn316 <zhangning2509@gmail.com>
2026-04-05 10:18:00 +0000

    1

--- app.py
+++ app.py
@@ -6,5 +6,22 @@ app = Flask(__name__)
 def index():
     return(render_template("index.html"))
 
+@app.route('/main', methods=['get','post'])
+def main():   
+    return (render_template('main.html') )
+
+@app.route('/ethics', methods=['get','post'])
+def ethics():   
+    return (render_template('ethics.html') )
+
+@app.route("/wrong", methods=["GET", "POST"])
+def wrong():
+    return render_template("wrong.html")
+
+
+@app.route("/correct", methods=["GET", "POST"])
+def correct():
+    return render_template("correct.html")
+
 if __name__=="__main__":
     app.run()
