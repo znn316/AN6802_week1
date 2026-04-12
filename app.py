d8bb98def2ee5fa9b5412a8db7952fd44120c40c
znn316 <zhangning2509@gmail.com>
2026-04-12 09:40:38 +0000

    1

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, request
+import joblib
 
+model = joblib.load("foodexp.pkl")
+#model.predict([[5000]])
 app = Flask(__name__)
 
 @app.route("/", methods=["get","post"])
@@ -22,6 +25,18 @@ def wrong():
 @app.route("/correct", methods=["GET", "POST"])
 def correct():
     return render_template("correct.html")
+
+@app.route('/econ', methods=['get','post'])
+def econ():   
+    return (render_template('econ.html') )
+
+@app.route("/foodExp",methods=["get","post"])
+def foodExp():
+    q = float(request.form.get("q"))
+    r = model.predict([[q]])
+    return(render_template("foodExp.html",r=r[0][0]))
+
+
 
 if __name__=="__main__":
     app.run()
